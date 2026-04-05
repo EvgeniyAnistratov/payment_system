3236c2ae0317554eeb207cd00a1678be93ed464d
EvgeniyAnistratov <evgeneanistratov@yandex.ru>
2026-04-05 19:47:38 +0300

    feat: add utils

---
+++ payment_system/utils/passwords.py
@@ -0,0 +1,17 @@
+import bcrypt
+
+
+_STR_ENCODING = "utf-8"
+
+
+def make_password(raw_password):
+    byte_password = raw_password.encode(_STR_ENCODING)
+    salt = bcrypt.gensalt()
+    return bcrypt.hashpw(byte_password, salt).decode(_STR_ENCODING)
+
+
+def compare_passwords(provided_password, hashed_password):
+    return bcrypt.checkpw(
+        provided_password.encode(_STR_ENCODING),
+        hashed_password.encode(_STR_ENCODING)
+    )
